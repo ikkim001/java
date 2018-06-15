54f3bccad36d25c399aeb3f8d70c22ec5bb11064
USER <USER@INET-01>
2018-06-15 11:45:55 +0900

    6-15

---
+++ 0615/src/SJ.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class SJ {
+	static class MyGUI extends JFrame {
+		MyGUI() {
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			
+			setSize(300,500);
+			setTitle("연습");
+			
+			setLayout(new BorderLayout());
+			JButton button = new JButton("과목");
+			this.add(button);
+			setVisible(true);
+			
+		}
+	}
+	
+
+}
